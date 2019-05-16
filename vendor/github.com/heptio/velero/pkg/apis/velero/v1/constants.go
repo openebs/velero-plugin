65d90f138abf7d0c5e5ba7e6f6ac2d9f6989e220
mayank <mayank.patel@mayadata.io>
2019-05-16 16:45:40 +0530

    Adding vendor files
    Signed-off-by: mayank <mayank.patel@mayadata.io>

--- vendor/github.com/heptio/velero/pkg/apis/velero/v1/constants.go
+++ vendor/github.com/heptio/velero/pkg/apis/velero/v1/constants.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 the Heptio Ark contributors.
+Copyright 2017 the Velero contributors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -28,14 +28,6 @@ const (
 	// MetadataDir is a top-level directory expected in backups which contains
 	// files that store metadata about the backup, such as the backup version.
 	MetadataDir = "metadata"
-
-	// RestoreLabelKey is the label key that's applied to all resources that
-	// are created during a restore. This is applied for ease of identification
-	// of restored resources. The value will be the restore's name.
-	//
-	// This label is DEPRECATED as of v0.10 and will be removed entirely as of
-	// v1.0 and replaced with RestoreNameLabel ("velero.io/restore-name").
-	RestoreLabelKey = "velero-restore"
 
 	// ClusterScopedDir is the name of the directory containing cluster-scoped
 	// resources within a Velero backup.
