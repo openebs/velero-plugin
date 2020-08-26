8a4f0b1d2602d401b3687c77c21e14a4e37cac16
sai chaithanya <sai.chaithanya@mayadata.io>
2020-08-26 17:24:17 +0530

    fix(csi, local restore): add support for local restore of cStor CSI volume (#108)
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- pkg/cstor/pv_operation.go
+++ pkg/cstor/pv_operation.go
@@ -34,22 +34,27 @@ const (
 func (p *Plugin) updateVolCASInfo(data []byte, volumeID string) error {
 	var cas v1alpha1.CASVolume
 
-	err := json.Unmarshal(data, &cas)
-	if err != nil {
-		return err
-	}
-
 	vol := p.volumes[volumeID]
 	if vol == nil {
 		return errors.Errorf("Volume{%s} not found in volume list", volumeID)
 	}
-	vol.iscsi = v1.ISCSIPersistentVolumeSource{
-		TargetPortal: cas.Spec.TargetPortal,
-		IQN:          cas.Spec.Iqn,
-		Lun:          cas.Spec.Lun,
-		FSType:       cas.Spec.FSType,
-		ReadOnly:     false,
+
+	if !vol.isCSIVolume {
+		err := json.Unmarshal(data, &cas)
+		if err != nil {
+			return err
+		}
+
+		vol.iscsi = v1.ISCSIPersistentVolumeSource{
+			TargetPortal: cas.Spec.TargetPortal,
+			IQN:          cas.Spec.Iqn,
+			Lun:          cas.Spec.Lun,
+			FSType:       cas.Spec.FSType,
+			ReadOnly:     false,
+		}
+		return nil
 	}
+	//NOTE: As of now no need to handle restore response for cStor CSI volumes
 	return nil
 }
 
