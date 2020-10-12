openebs/velero-plugin

commit a4fe177bfd9580808f928a08236ab7928be512cb

feat(zfspv): use schedule name to identify the scheduled backup (#124)
User can create a backup using same name (or format) as scheduled backup, this
will cause restore to break as it simply does the lookup based on scheduled name.
There is no difference in the way we take the full backup and scheduled incremental
backup.

Here, we are adding schedule name in the backup file name so that we can list based on
that while doing the restore. The user created backup using schedule name will not be
considered as it is not the scheduled backup so it will not be considered while doing restore.

Signed-off-by: Pawan <pawan@mayadata.io>

pkg/zfs/plugin/backup.go
@@ -70,6 +70,11 @@ func (p *Plugin) deleteBackup(snapshotID string) error {
 }
 
 func (p *Plugin) getPrevSnap(volname, schdname string) (string, error) {
+	if p.incremental < 1 || len(schdname) == 0 {
+		// not an incremental backup, take the full backup
+		return "", nil
+	}
+
 	listOptions := metav1.ListOptions{
 		LabelSelector: VeleroSchdKey + "=" + schdname + "," + VeleroVolKey + "=" + volname,
 	}
@@ -111,7 +116,7 @@ func (p *Plugin) getPrevSnap(volname, schdname string) (string, error) {
 }
 
 func (p *Plugin) createBackup(vol *apis.ZFSVolume, schdname, snapname string, port int) (string, error) {
-	bkpname := utils.GenerateSnapshotID(vol.Name, snapname)
+	bkpname := utils.GenerateResourceName(vol.Name, snapname)
 
 	p.Log.Debugf("zfs: creating ZFSBackup vol = %s bkp = %s schd = %s", vol.Name, bkpname, schdname)
 
@@ -220,7 +225,7 @@ func (p *Plugin) doBackup(volumeID string, snapname string, schdname string, por
 		return "", errors.Errorf("zfs: err pv is not claimed")
 	}
 
-	filename := p.cl.GenerateRemoteFilename(volumeID, snapname)
+	filename := p.cl.GenerateRemoteFileWithSchd(volumeID, schdname, snapname)
 	if filename == "" {
 		return "", errors.Errorf("zfs: error creating remote file name for backup")
 	}
@@ -271,7 +276,7 @@ func (p *Plugin) doBackup(volumeID string, snapname string, schdname string, por
 	}
 
 	// generate the snapID
-	snapID := utils.GenerateSnapshotID(volumeID, snapname)
+	snapID := utils.GenerateSnapshotID(volumeID, schdname, snapname)
 
 	p.Log.Debugf("zfs: backup done vol %s bkp %s snapID %s", volumeID, bkpname, snapID)
 

pkg/zfs/utils/utils.go
@@ -19,7 +19,6 @@ package utils
 import (
 	"net"
 	"strings"
-	"time"
 
 	"github.com/gofrs/uuid"
 	"github.com/pkg/errors"
@@ -49,20 +48,31 @@ func GetServerAddress() (string, error) {
 	return "", errors.New("error: fetching the interface")
 }
 
-func GenerateSnapshotID(volumeID, backupName string) string {
+func GenerateResourceName(volumeID, backupName string) string {
 	return volumeID + IdentifierKey + backupName
 }
 
-// GetInfoFromSnapshotID return backup name and volume id from the given snapshotID
-func GetInfoFromSnapshotID(snapshotID string) (volumeID, backupName string, err error) {
+func GenerateSnapshotID(volumeID, schdname, backupName string) string {
+	return volumeID + IdentifierKey + schdname + IdentifierKey + backupName
+}
+
+// GetInfoFromSnapshotID return backup, schdname and volume id from the given snapshotID
+func GetInfoFromSnapshotID(snapshotID string) (volumeID, schdname, backupName string, err error) {
 	s := strings.Split(snapshotID, IdentifierKey)
-	if len(s) != 2 {
+
+	if len(s) == 2 {
+		// backward compatibility, old backups
+		volumeID = s[0]
+		backupName = s[1]
+		schdname = ""
+	} else if len(s) == 3 {
+		volumeID = s[0]
+		schdname = s[1]
+		backupName = s[2]
+	} else {
 		err = errors.New("invalid snapshot id")
 		return
 	}
-
-	volumeID = s[0]
-	backupName = s[1]
 
 	if volumeID == "" || backupName == "" {
 		err = errors.Errorf("invalid volumeID=%s backupName=%s", volumeID, backupName)
@@ -78,23 +88,4 @@ func GetRestorePVName() (string, error) {
 	}
 
 	return RestorePrefix + nuuid.String(), nil
-}
-
-// GetScheduleName return the schedule name for the given backup
-// It will check if backup name have 'bkp-20060102150405' format
-func GetScheduleName(backupName string) string {
-	// for non-scheduled backup, we are considering backup name as schedule name only
-	schdName := backupName
-
-	// If it is scheduled backup then we need to get the schedule name
-	splitName := strings.Split(backupName, "-")
-	if len(splitName) >= 2 {
-		_, err := time.Parse("20060102150405", splitName[len(splitName)-1])
-		if err != nil {
-			// last substring is not timestamp, so it is not generated from schedule
-			return schdName
-		}
-		schdName = strings.Join(splitName[0:len(splitName)-1], "-")
-	}
-	return schdName
 }

pkg/clouduploader/operation.go
@@ -155,6 +155,24 @@ func (c *Conn) GenerateRemoteFilename(file, backup string) string {
 	return c.backupPathPrefix + "/" + backupDir + "/" + backup + "/" + c.prefix + "-" + file + "-" + backup
 }
 
+// GenerateRemoteFileWithSchd will create a file-name specific for given backup and schedule name
+func (c *Conn) GenerateRemoteFileWithSchd(file, schdname, backup string) string {
+	filePath := backupDir + "/" + backup + "/" + c.prefix
+	if len(schdname) > 0 {
+		filePath += "-" + schdname
+	}
+
+	if c.backupPathPrefix == "" {
+		return filePath + "-" + file + "-" + backup
+	}
+
+	return c.backupPathPrefix + "/" + filePath + "-" + file + "-" + backup
+}
+
+func (c *Conn) GetFileNameWithSchd(file, schdname string) string {
+	return schdname + "-" + file
+}
+
 // ConnStateReset resets the channel and exit server flag
 func (c *Conn) ConnStateReset() {
 	ch := make(chan bool, 1)

pkg/zfs/plugin/restore.go
@@ -112,10 +112,10 @@ func (p *Plugin) createVolume(pvname string, bkpname string, bkpZV *apis.ZFSVolu
 	return vol, nil
 }
 
-func (p *Plugin) restoreZFSVolume(pvname, bkpname string) (*apis.ZFSVolume, error) {
+func (p *Plugin) restoreZFSVolume(pvname, schdname, bkpname string) (*apis.ZFSVolume, error) {
 	bkpZV := &apis.ZFSVolume{}
 
-	filename := p.cl.GenerateRemoteFilename(pvname, bkpname)
+	filename := p.cl.GenerateRemoteFileWithSchd(pvname, schdname, bkpname)
 
 	data, ok := p.cl.Read(filename + ".zfsvol")
 	if !ok {
@@ -208,7 +208,7 @@ func (p *Plugin) startRestore(zv *apis.ZFSVolume, bkpname string, port int) (str
 	node := zv.Spec.OwnerNodeID
 	serverAddr := p.remoteAddr + ":" + strconv.Itoa(port)
 	zfsvol := zv.Name
-	rname := utils.GenerateSnapshotID(zfsvol, bkpname)
+	rname := utils.GenerateResourceName(zfsvol, bkpname)
 
 	rstr, err := restorebuilder.NewBuilder().
 		WithName(rname).
@@ -242,8 +242,8 @@ func (p *Plugin) doDownload(wg *sync.WaitGroup, filename string, port int) {
 	close(*p.cl.ConnReady)
 }
 
-func (p *Plugin) dataRestore(zv *apis.ZFSVolume, pvname, bkpname string, port int) error {
-	filename := p.cl.GenerateRemoteFilename(pvname, bkpname)
+func (p *Plugin) dataRestore(zv *apis.ZFSVolume, pvname, schdname, bkpname string, port int) error {
+	filename := p.cl.GenerateRemoteFileWithSchd(pvname, schdname, bkpname)
 	if filename == "" {
 		return errors.Errorf("zfs: Error creating remote file name for restore")
 	}
@@ -285,12 +285,17 @@ func (p *Plugin) dataRestore(zv *apis.ZFSVolume, pvname, bkpname string, port in
 	return nil
 }
 
-func (p *Plugin) getSnapList(pvname, bkpname string) ([]string, error) {
-	var list []string
+func (p *Plugin) getSnapList(pvname, schdname, bkpname string) ([]string, error) {
+	list := []string{bkpname}
 
-	schdname := utils.GetScheduleName(bkpname)
+	if p.incremental < 1 || len(schdname) == 0 {
+		// not an incremental backup, return the list having bkpname
+		return list, nil
+	}
 
-	snapList, err := p.cl.GetSnapListFromCloud(pvname, schdname)
+	filename := p.cl.GetFileNameWithSchd(pvname, schdname)
+
+	snapList, err := p.cl.GetSnapListFromCloud(filename, schdname)
 	if err != nil {
 		return list, err
 	}
@@ -322,12 +327,12 @@ func (p *Plugin) getSnapList(pvname, bkpname string) ([]string, error) {
 }
 
 func (p *Plugin) doRestore(snapshotID string, port int) (string, error) {
-	pvname, bkpname, err := utils.GetInfoFromSnapshotID(snapshotID)
+	pvname, schdname, bkpname, err := utils.GetInfoFromSnapshotID(snapshotID)
 	if err != nil {
 		return "", err
 	}
 
-	bkpList, err := p.getSnapList(pvname, bkpname)
+	bkpList, err := p.getSnapList(pvname, schdname, bkpname)
 	if err != nil {
 		return "", err
 	}
@@ -338,7 +343,7 @@ func (p *Plugin) doRestore(snapshotID string, port int) (string, error) {
 
 	p.Log.Debugf("zfs: backup list for restore %v", bkpList)
 
-	zv, err := p.restoreZFSVolume(pvname, bkpname)
+	zv, err := p.restoreZFSVolume(pvname, schdname, bkpname)
 	if err != nil {
 		p.Log.Errorf("zfs: restore ZFSVolume failed vol %s bkp %s err %v", pvname, bkpname, err)
 		return "", err
@@ -346,7 +351,7 @@ func (p *Plugin) doRestore(snapshotID string, port int) (string, error) {
 
 	// attempt the incremental restore, will resote single backup if it is not a incremental backup
 	for _, bkp := range bkpList {
-		err = p.dataRestore(zv, pvname, bkp, port)
+		err = p.dataRestore(zv, pvname, schdname, bkp, port)
 
 		if err != nil {
 			// delete the volume

commit 0f956ea239bd7db6d82bd422cff98de3caffa575

fix(zfspv): fixing the backup deletion
We use pvname and snapname for creating the backup. Now since we are returning schedule
name also in snapshotID, so While deleting the ZFSBackup CR we should use the pvname and snapname.

We broke this in https://github.com/openebs/velero-plugin/pull/125.

Also fixed the backup compatibility for older backups where we were not fetching the schedule name.
We should fetch the schedule name from the backup name using the naming format.

Signed-off-by: Pawan <pawan@mayadata.io>

pkg/zfs/plugin/backup.go
@@ -61,7 +61,13 @@ func (p *Plugin) uploadZFSVolume(vol *apis.ZFSVolume, filename string) error {
 
 // deleteBackup deletes the backup resource
 func (p *Plugin) deleteBackup(snapshotID string) error {
-	err := bkpbuilder.NewKubeclient().WithNamespace(p.namespace).Delete(snapshotID)
+	pvname, _, snapname, err := utils.GetInfoFromSnapshotID(snapshotID)
+	if err != nil {
+		return err
+	}
+
+	bkpname := utils.GenerateResourceName(pvname, snapname)
+	err = bkpbuilder.NewKubeclient().WithNamespace(p.namespace).Delete(bkpname)
 	if err != nil {
 		p.Log.Errorf("zfs: Failed to delete the backup %s", snapshotID)
 	}

pkg/zfs/utils/utils.go
@@ -19,6 +19,7 @@ package utils
 import (
 	"net"
 	"strings"
+	"time"
 
 	"github.com/gofrs/uuid"
 	"github.com/pkg/errors"
@@ -64,7 +65,8 @@ func GetInfoFromSnapshotID(snapshotID string) (volumeID, schdname, backupName st
 		// backward compatibility, old backups
 		volumeID = s[0]
 		backupName = s[1]
-		schdname = ""
+		// for old backups fetch the schdeule from the bkpname
+		schdname = GetScheduleName(backupName)
 	} else if len(s) == 3 {
 		volumeID = s[0]
 		schdname = s[1]
@@ -88,4 +90,23 @@ func GetRestorePVName() (string, error) {
 	}
 
 	return RestorePrefix + nuuid.String(), nil
+}
+
+// GetScheduleName return the schedule name for the given backup
+// It will check if backup name have 'bkp-20060102150405' format
+func GetScheduleName(backupName string) string {
+	// for non-scheduled backup, we are considering backup name as schedule name only
+	schdName := ""
+
+	// If it is scheduled backup then we need to get the schedule name
+	splitName := strings.Split(backupName, "-")
+	if len(splitName) >= 2 {
+		_, err := time.Parse("20060102150405", splitName[len(splitName)-1])
+		if err != nil {
+			// last substring is not timestamp, so it is not generated from schedule
+			return schdName
+		}
+		schdName = strings.Join(splitName[0:len(splitName)-1], "-")
+	}
+	return schdName
 }